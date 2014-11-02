2a59f723c7acaa518d9986e2666cc0cdec28d887
rahul <rahul8590@gmail.com>
2014-11-02 05:42:17 -0500

    solved minesweeper problem. one endl made all the diff

--- cpp/10189.cpp
+++ cpp/10189.cpp
@@ -1,36 +1,95 @@
 #include<iostream>
-#include<array>
+
 using namespace std;
 
-int **const_matrix(int rows , int cols) {
-  int **mat = new int*[rows];
+
+char  **const_matrix(int rows , int cols) {
+  char **mat = new char*[rows];
   for (int i = 0 ; i < rows ; i++ ) {
-          mat [i] = new int [cols]; 
+          mat [i] = new char [cols]; 
           for (int j = 0 ; j < cols ; j++ ){
-            mat[i][j] = 2;
+            mat[i][j] = '0';
           }
   }
   return mat ;
 }
 
+char inchar(char num){
+	if (num == '*')
+		return num;
+
+	int val = num ;
+	val++;
+	return char(val);
+}
+
+
+
 int main() {
-	int rows, cols , **mat1;
+	int rows, cols , count = 1;
+	
 
 		while(cin >> rows >> cols) {   
-		mat1 = const_matrix(rows,cols);	
-		//auto mat1 = new int[rows][cols];
-		//array <int,2> mat1 = array<int,2>(rows,cols);
+			if (rows == 0 && cols == 0)
+				return 0;
+			if (count > 1)
+				cout << endl;
+
+		char **mat1 = const_matrix(rows,cols);	
+		
 		for (int i = 0 ; i < rows ; i++ ) {
-    for (int j = 0 ; j < cols; j++) {
-      cout << mat1[i][j] << " " ;
-    }
-    cout << endl;
+    		cin >> mat1[i] ;
+   	}
+		
+		for (int i = 0 ; i < rows ; i++ )
+    	for (int j = 0 ; j < cols; j++)
+    		if (mat1[i][j] == '.'){
+    			mat1[i][j] = '0' ; //Init to zero
+    			//cout <<"detected dots";
+    		}
+    		
+       	
+
+		for (int i = 0 ; i < rows ; i++ ) {
+    	for (int j = 0 ; j < cols; j++) {
+      	if (mat1[i][j] == '*') {
+      		if ( i - 1 >= 0 ) {
+      			mat1[i-1][j-1] = inchar(mat1[i-1][j-1]) ;  //Top left 
+      			mat1[i-1][j] = inchar(mat1[i-1][j]); //centre top
+      			mat1[i-1][j+1] =  inchar(mat1[i-1][j+1]) ; //Top right 
+      		}
+      		if (i+1 < rows) {
+      			mat1[i+1][j-1] = inchar(mat1[i+1][j-1]) ;  //Bottom left 
+      			mat1[i+1][j] = inchar(mat1[i+1][j]) ; //centre Bottom
+      			mat1[i+1][j+1] = inchar(mat1[i+1][j+1]) ; //Bottom right	
+      		}
+      	  if (j - 1 >= 0) {
+      	  	mat1[i][j-1] = inchar(mat1[i][j-1]) ;  //Same left
+      	  }
+      	  if ( j + 1 < cols ) {
+      	  	mat1[i][j+1] = inchar(mat1[i][j+1]);
+      	  }
+
+      	}
+
+    	}
    }
+
+   cout<<"Field"<<" "<<"#"<<count++<<":"<<endl;
+   //Printing the complete minesweep
+   for (int i = 0 ; i < rows ; i++ ) {
+    	for (int j = 0 ; j < cols; j++)
+    		cout << mat1[i][j] ;
+    	cout << endl;
+    }
+
+    
    //Code to delete the arrays 
-   for( int i=0; i<rows; ++i ) {
+   /*for( int i=0; i<rows; ++i ) {
   		delete[] mat1[i];
 		}
 		delete[] mat1;   
+		cout << "1st iteration is done";*/
  }
 	return 0;
 }
